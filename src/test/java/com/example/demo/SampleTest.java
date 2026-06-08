bbdfe8639d53a2e5acbf6a1b2d870b16f2b57a3c
NishizakiYuki <nishizaki.yuki@compass-japan.com>
2026-06-08 10:08:43 +0900

    ほんまにできんの？

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
